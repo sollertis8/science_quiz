c3bb868d9fa3ebfed300c5939530dbab87ef6e77
sollertis8 <mizumedia@gmail.com>
2017-08-16 16:03:55 -0400

    Add files via upload

---
+++ pseudomain.js
@@ -0,0 +1,49 @@
+function handleStartButtonClicked() {
+	// this function will be responsible for handling what happens 
+	// when the "Start" button is clicked
+	console.log('`handleStartButtonClicked` ran');
+}
+
+function handleAnswerSelected(answer) {
+	// this function will be responsible for handling what happens 
+	// when an answer is selected
+	// correctCount = 0;
+	console.log('`handleAnswerSelected` ran');
+}
+
+function handleNextButtonClicked(questionNum) {
+	// this function will be responsible for 
+	// handling what happens when the "Next" button is clicked
+	console.log('`handleNextButtonClicked` ran');
+}
+
+function handleResultsButtonClicked() {
+	// this function will be responsible for handling what happens 
+	// when the "Results" button is clicked
+	console.log('`handleResultsButtonClicked` ran');
+}
+
+function renderStartPage() {
+	// this function will be responsible for rendering the start page 
+	// in the DOM
+	console.log('`renderStartPage` ran');
+}
+
+function renderQuestionPage(questionNum) {
+	// this function will be responsible for rendering the question page
+	// in the DOM
+	console.log('`renderQuestionPage` ran');
+}
+
+function renderResultsPage(results) {
+	// this function will be responsible for rendering the results page 
+	// in the DOM
+	console.log('`renderResultsPage` ran');
+}
+
+function handleScienceQuiz() {
+	// render the start page
+	// handle answers
+}
+
+
